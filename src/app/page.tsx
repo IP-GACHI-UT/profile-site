a259732464f63f5e7df3da1efaf34111230a612b
Radon0 <ryoto.enjoy@outlook.jp>
2026-03-21 00:21:17 +0900

    feat: 記事一覧の作成
    ・記事をカード表示で並べることができます。

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import ArticleList from './components/ArticleList';
+
 export default function Home() {
   return (
     <main>
       <h1 className="text-2xl font-bold">Profile</h1>
+
+      <ArticleList />
     </main>
   );
 }
